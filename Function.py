93d6f43c2a8cd20731f677c878a15547cf4fcce0
Uvarovfk <uvarov.fk@phystech.edu>
2025-03-27 11:49:54 +0300

    new

--- Function.py
+++ Function.py
@@ -1,24 +1,29 @@
 from typing import Callable, List
 import numpy as np
 from calculate_delay import calculate_delay
-def Function(T: float, pulse: Callable[[float], complex], w: float, Theta: List[List[float]], G: List[List[float]],
-             M: List[List[List[float]]], Tstart:float, dT: float, dis: float, fx: Callable[[float], float],
+
+
+def function(t: float,
+             pulse: Callable[[float, float], complex],
+             w: float, theta_matrix: List[List[float]], g_matrix: List[List[float]], m_matrix: List[List[List[float]]],
+             t_start: float, d_t: float, dis: float, fx: Callable[[float], float],
              fy: Callable[[float], float], fz: Callable[[float], float]) -> List[List[complex] | int]:
 
     """
-    Функция возращает кортеж из 3 элементов:
+        Функция возращает список из 3 элементов:
      1) список, состоящий из оцифровок суммарной амплитуды принятого сигнала
      2) длину принятого сигнала в отсчетах
      3) номер отсчета начала приема сигнала
 
-    :param T: момент времени излучения импульса
+
+    :param: theta_matrix: матрица состоящая из сдвигов фаз для каждой точки поверхности
+    :param: g_matrix: матрица состоящая из коэффициента усиления для каждой точки поверхности
+    :param t: момент времени излучения импульса
     :param w: частота сигнала
     :param pulse: функция, описывающая импульс радиолокатора
-    :param: Theta: матрица состоящая из сдвигов фаз для каждой точки поверхности
-    :param: G: матрица состоящая из коэффициента усиления для каждой точки поверхности
-    :param M: матрица с координатами(X,Y,Z) каждого элемента матрицы
-    :param Tstart: начальный момент времени
-    :param dT: время излучения импульса
+    :param: theta_matrix: матрица состоящая из сдвигов фаз для каждой точки поверхности
+    :param t_start: начальный момент времени
+    :param d_t: время излучения импульса
     :param dis: период дискретизации
     :param fx: Функция интерполирующая координату x
     :param fy: Функция интерполирующая координату y
@@ -26,30 +31,35 @@ def Function(T: float, pulse: Callable[[float], complex], w: float, Theta: List[
 
     """
 
-    l = 0  # счетчик
+    l1 = 0  # счетчик
     r = 0  # счетчик
-    n = len(M)
-    m = len(M[0])
-    Amplitude = np.zeros((n * m, int(dT / dis), 2))  # матрица, содеращая в себе амплитуду и номер отсчета времени приема принятого отраженного сигнала от каждой(всего nxm) точки
+    n = len(m_matrix)
+    m = len(m_matrix[0])
 
-    v = (fx(T), fy(T), fz(T))  # интерполированное значение координат положения самолета в момент времени T
+    # матрица, содеращая в себе амплитуду и номер отсчета времени приема принятого сигнала от каждой(всего nxm) точки
+    amplitude = np.zeros((n * m, int(d_t / dis), 2))
 
-    Time_Global = [0] * 10000  # Вводится список состоящий из мементов времени
+    vector = [fx(t), fy(t), fz(t)]  # интерполированное значение координат положения самолета в момент времени T
+
+    time_global: List[float] = [0] * 10000  # Вводится список состоящий из мементов времени
 
     for k in range(10000):
-        Time_Global[k] = Tstart + dis * k
+        time_global[k] = t_start + dis * k
 
     # запись амплитуды принятого отраженного сигнала в А и соотвеатвующий ей номер отсчета k
 
     for i in range(n):
         for j in range(m):
-            delay = calculate_delay(v, M[i][j], 1000.)
-            for k in range(int((T-Tstart)/dis), int((T-Tstart)/dis)+int(delay/dis)+int(dT/dis)+1,1):
-                if T + delay <= Time_Global[k] and Time_Global[k] <= T + delay + dT:
-                    Amplitude[l][r][0] = k
-                    Amplitude[l][r][1] = pulse(Time_Global[k] - T - delay, w) * np.exp(1j * Theta[i][j]) * G[i][j]  # записывается амплитуда принятого сигнала с учетом сдвига по фазе и коэффициента усиления точки поверхности
+            delay = calculate_delay(m_matrix[i][j], vector,  1000.)
+            t1, t2, t3 = (t - t_start) / dis, delay / dis, d_t / dis
+            for k in range(int(t1), int(t1) + int(t2) + int(t3) + 1, 1):
+                if t + delay <= time_global[k] and time_global[k] <= t + delay + d_t:
+                    amplitude[l1][r][0] = k
+                    # записывается амплитуда сигнала с учетом сдвига по фазе и коэффициента усиления точки поверхности
+                    pulse1 = pulse(time_global[k] - t - delay, w)
+                    amplitude[l1][r][1] = pulse1 * np.exp(1j * theta_matrix[i][j]) * g_matrix[i][j]
                     r = r + 1
-            l = l + 1
+            l1 += 1
             r = 0
     max_num_samples = 0  # максимальный номер отсчета
     min_num_samples = None  # минимальный номер отсчета
@@ -57,13 +67,13 @@ def Function(T: float, pulse: Callable[[float], complex], w: float, Theta: List[
     # суммирование амплитуд, соотвествующих одному и тому же моменту времени k
 
     for i in range(n * m):
-        for j in range(int(dT / dis)):
-            if Amplitude[i][j][0] >= max_num_samples:
-                max_num_samples = Amplitude[i][j][0]
+        for j in range(int(d_t / dis)):
+            if amplitude[i][j][0] >= max_num_samples:
+                max_num_samples = amplitude[i][j][0]
 
-            if min_num_samples is None or Amplitude[i][j][0] <= min_num_samples:
+            if min_num_samples is None or amplitude[i][j][0] <= min_num_samples:
                 if min_num_samples != 0:
-                    min_num_samples = Amplitude[i][j][0]
+                    min_num_samples = amplitude[i][j][0]
 
     u = int(max_num_samples + 1) - int(min_num_samples)  # длительность приема одного импульса в количестве отсчетов
 
@@ -74,9 +84,9 @@ def Function(T: float, pulse: Callable[[float], complex], w: float, Theta: List[
 
     for i in range(int(min_num_samples), int(max_num_samples + 1), 1):
         for j in range(n * m):
-            for k in range(int(dT / dis)):
-                if int(Amplitude[j][k][0]) == i:
-                    sum_amplitude[s] = sum_amplitude[s] + Amplitude[j][k][1]
+            for k in range(int(d_t / dis)):
+                if int(amplitude[j][k][0]) == i:
+                    sum_amplitude[s] = sum_amplitude[s] + amplitude[j][k][1]
         s = s + 1
     # на выходе функции 1) список амплитуд, каждая следующая амплитуда соответсвует следующему номеру отсчета k
     # 2) количество отсчетов 3) номер минимального отсчета(первого отсчета)
--- program.py
+++ program.py
@@ -1,19 +1,17 @@
-from Function import Function
+from Function import function
 from sinusoidal_pulse import sinusoidal_pulse
-import numpy as np
 from scipy import interpolate
-from numpy import random
 import matplotlib.pyplot as plt
 
-n = 40 # количество строк матрицы G, M и Thetta
-m = 10 # количество столбцов матрицы G, M и Thetta
+n = 40  # количество строк матрицы G, M и Thetta
+m = 10  # количество столбцов матрицы G, M и Thetta
 
-
-
-G_matrix = [[0 for z in range(m)] for y in range(n)]  # матрица значений G(коэффициента усиления от каждого элемента поверхности)
-M_matrix = [[[0 for z in range(3)] for y in range(m)] for x in range(n)] # матрица координат точек на поверхности размером nXm, содержащая 3 координаты: X,Y,Z
-Theta_matrix = [[0 for z in range(m)] for y in range(n)] # матрица значений Thetta(сдвига фаз в градусах от каждого элемента поверхности)
-
+G_matrix = [[0 for z1 in range(m)] for y1 in
+            range(n)]  # матрица значений G(коэффициента усиления от каждого элемента поверхности)
+M_matrix = [[[0 for z2 in range(3)] for y2 in range(m)] for x2 in
+            range(n)]  # матрица координат точек на поверхности размером nXm, содержащая 3 координаты: X,Y,Z
+Theta_matrix = [[0 for z3 in range(m)] for y3 in
+                range(n)]  # матрица значений Thetta(сдвига фаз в градусах от каждого элемента поверхности)
 
 # заполняем матрицу M_matrix:
 for i in range(n):
@@ -34,12 +32,12 @@ for i in range(n):
     for j in range(m):
         Theta_matrix[i][j] = 1
 
-N = 10 # количество точек на траектории
+N = 10  # количество точек на траектории
 
-X_Plane = [0]*N # Список из X компоненты координат положения самолета
-Y_Plane = [0]*N # Список из Y компоненты координат положения самолета
-Z_Plane = [0]*N # Список из X компоненты координат положения самолета
-t_Plane = [0]*N # Список моментов времени, которым соответсвуют X,Y,Z координаты
+X_Plane = [0] * N  # Список из X компоненты координат положения самолета
+Y_Plane = [0] * N  # Список из Y компоненты координат положения самолета
+Z_Plane = [0] * N  # Список из X компоненты координат положения самолета
+t_Plane = [0] * N  # Список моментов времени, которым соответсвуют X,Y,Z координаты
 
 # заполняем координаты положения самолета
 for i in range(N):
@@ -59,11 +57,11 @@ fz = interpolate.interp1d(t_Plane, Z_Plane, kind='cubic')
 
 # строим график
 
-a1 = Function(5, sinusoidal_pulse, 0.1/0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[1]
-a2 = Function(5, sinusoidal_pulse, 0.1/0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[2]
-a3 = Function(5, sinusoidal_pulse, 0.1/0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[0]
-go = [0]*a1
+a1 = function(5, sinusoidal_pulse, 0.1 / 0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[1]
+a2 = function(5, sinusoidal_pulse, 0.1 / 0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[2]
+a3 = function(5, sinusoidal_pulse, 0.1 / 0.01, Theta_matrix, G_matrix, M_matrix, 4., .1, 0.01, fx, fy, fz)[0]
+go = [0] * a1
 for i in range(a1):
-    go[i] = i+a2
+    go[i] = i + a2
 plt.plot(go, a3)
 plt.show()
