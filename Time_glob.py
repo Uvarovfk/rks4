70e701b37889b96c2ec0c20c87f39f7af8dcd13d
Uvarovfk <uvarov.fk@phystech.edu>
2025-03-27 15:08:25 +0300

    new

--- Time_glob.py
+++ Time_glob.py
@@ -1,2 +1,9 @@
 def time_glob(index: int, t_start: float, dis: float) -> float:
+    """
+        Функция возвращает момент времени, соответсвующий отсчету index
+
+        :param index: номер отсчета
+        :param t_start: начальный момент времени
+        :param dis: период дискретизации
+        """
     return t_start + index * dis
